72abd1435eb266879cde91e4d23d47d03cf07e5e
Bogomil Kolarov <kbogomil@gmail.com>
2018-04-23 20:24:25 +0300

    Library module init

---
+++ magicforms/src/androidTest/java/magicforms/libs/android/dephinera/com/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package magicforms.libs.android.dephinera.com;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("magicforms.libs.android.dephinera.com.test", appContext.getPackageName());
+    }
+}
